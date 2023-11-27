342d76ccf5f2f671488f9f0078b1800ad7ddde1f
liuqing <330663384@qq.com>
2023-11-28 00:05:59 +0800

    test push

---
+++ src/navigation/component/TabBar.tsx
@@ -0,0 +1,12 @@
+import { BottomTabBar } from "@react-navigation/bottom-tabs";
+import React from "react";
+import { View } from "react-native";
+
+export function TabBar(){
+  return (
+    <View>
+
+    </View>
+    // <BottomTabBar state={} descriptors={} navigation={} insets={}/>
+  )
+}
